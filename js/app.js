0b7d77a8547bececb389c478db752862440fe3a8
Daniel Ferreira <90437030+daniells56@users.noreply.github.com>
2023-05-17 17:03:53 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,20 +2,40 @@
 
 document.onreadystatechange = function() {
     if (document.readyState !== "complete") {
-        document.querySelector("body").style.visibility = "hidden";
+        window.scrollTo(0, 0);
+        document.querySelector(".site").style.visibility = "hidden";
+        document.querySelector(".site").style.display = "none";
         document.querySelector("body").style.overflow = "hidden";
-        document.querySelector(".title2").style.visibility = "visible";
-        document.querySelector(".load-txt").style.visibility = "visible";
-        document.querySelector(".coffee").style.visibility = "visible";
+
+        document.querySelector(".loadingpae").style.display = "initial";
+
+
+
     } else {
       setInterval(function() {
-        document.querySelector("body").style.visibility = "initial";
+        document.querySelector(".site").style.visibility = "initial";
         document.querySelector("body").style.overflow = "initial";
-        document.querySelector(".title2").style.visibility = "hidden";
-        document.querySelector(".load-txt").style.visibility = "hidden";
-        document.querySelector(".coffee").style.visibility = "hidden";
-      }, 1000);
+        document.querySelector(".title2").style.display = "none";
+        document.querySelector(".load-txt").style.display = "none";
+        document.querySelector(".coffee").style.display = "none";
+        document.querySelector(".site").style.display = "initial";
+      }, 2000);
 
     }
 };
 
+
+function inicio() {
+  window.scrollTo(0, 0);
+
+}
+
+function Projetos() {
+  window.scrollTo(720, 720);
+
+}
+
+function abboutt() {
+  window.scrollTo(1400, 1400);
+
+}
